e0ed64a8f68bc045475f2596cca1201c772dccff
Jelena Sehovac <jelena.sehovac@chipglobe.com>
2022-03-28 16:30:46 +0200

    Read data from touch controller only if display is touched

--- drivers/touch/lv_ft6236.c
+++ drivers/touch/lv_ft6236.c
@@ -53,42 +53,46 @@ FT6x36Error_t lv_ft6236_init() {
 }
 
 void lv_ft6236_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
-    uint8_t data_buf[5] = {0, 0, 0, 0, 0}; // 1 byte status, 2 bytes X, 2 bytes Y
+    /* Read coordinates only if display is touched */
+    if (ft6236_is_touched()) {
+        /* Display is touched, read coordinates */
+        uint8_t data_buf[5] = {0, 0, 0, 0, 0}; // 1 byte status, 2 bytes X, 2 bytes Y
 
-    FT6x36Error_t ret = ft6236_i2c_read(FT6X36_TD_STAT_REG, &data_buf[0], 5);
-    if (ret != FT6x36_NO_ERROR) {
-        OT_API_CALL(otCliOutputFormat(ANSI_COLOR_RED "(ERROR) (%s): Error talking to touch IC: %d" ANSI_COLOR_RESET "\r\n", pcTaskGetName(NULL), ret));
-    }
-
-    uint8_t touch_pnt_cnt = data_buf[0]; // Number of detected touch points
-
-    if (ret != FT6x36_NO_ERROR || touch_pnt_cnt != 1) { // Ignore no touch & multi touch
-        if ( touch_inputs.current_state != LV_INDEV_STATE_REL) {
-            touch_inputs.current_state = LV_INDEV_STATE_REL;
+        FT6x36Error_t ret = ft6236_i2c_read(FT6X36_TD_STAT_REG, &data_buf[0], 5);
+        if (ret != FT6x36_NO_ERROR) {
+            OT_API_CALL(otCliOutputFormat(ANSI_COLOR_RED "(ERROR) (%s): Error talking to touch IC: %d" ANSI_COLOR_RESET "\r\n", pcTaskGetName(NULL), ret));
         }
+
+        uint8_t touch_pnt_cnt = data_buf[0]; // Number of detected touch points
+
+        if (ret != FT6x36_NO_ERROR || touch_pnt_cnt != 1) { // Ignore no touch & multi touch
+            if ( touch_inputs.current_state != LV_INDEV_STATE_REL) {
+                touch_inputs.current_state = LV_INDEV_STATE_REL;
+            }
+            data->point.x = touch_inputs.last_x;
+            data->point.y = touch_inputs.last_y;
+            data->state = touch_inputs.current_state;
+            return;
+        }
+
+        touch_inputs.current_state = LV_INDEV_STATE_PR;
+        touch_inputs.last_x = ((data_buf[1] & FT6X36_MSB_MASK) << 8) | (data_buf[2] & FT6X36_LSB_MASK);
+        touch_inputs.last_y = ((data_buf[3] & FT6X36_MSB_MASK) << 8) | (data_buf[4] & FT6X36_LSB_MASK);
+
+#if CONFIG_LV_FT6X36_SWAPXY
+        int16_t swap_buf = touch_inputs.last_x;
+        touch_inputs.last_x = touch_inputs.last_y;
+        touch_inputs.last_y = swap_buf;
+#endif
+#if CONFIG_LV_FT6X36_INVERT_X
+        touch_inputs.last_x =  LV_HOR_RES - touch_inputs.last_x;
+#endif
+#if CONFIG_LV_FT6X36_INVERT_Y
+        touch_inputs.last_y = LV_VER_RES - touch_inputs.last_y;
+#endif
         data->point.x = touch_inputs.last_x;
         data->point.y = touch_inputs.last_y;
         data->state = touch_inputs.current_state;
-        return;
+        OT_API_CALL(otCliOutputFormat("(DEBUG) (%s): X = %u Y = %u\r\n", pcTaskGetName(NULL), data->point.x, data->point.y));
     }
-
-    touch_inputs.current_state = LV_INDEV_STATE_PR;
-    touch_inputs.last_x = ((data_buf[1] & FT6X36_MSB_MASK) << 8) | (data_buf[2] & FT6X36_LSB_MASK);
-    touch_inputs.last_y = ((data_buf[3] & FT6X36_MSB_MASK) << 8) | (data_buf[4] & FT6X36_LSB_MASK);
-
-#if CONFIG_LV_FT6X36_SWAPXY
-    int16_t swap_buf = touch_inputs.last_x;
-    touch_inputs.last_x = touch_inputs.last_y;
-    touch_inputs.last_y = swap_buf;
-#endif
-#if CONFIG_LV_FT6X36_INVERT_X
-    touch_inputs.last_x =  LV_HOR_RES - touch_inputs.last_x;
-#endif
-#if CONFIG_LV_FT6X36_INVERT_Y
-    touch_inputs.last_y = LV_VER_RES - touch_inputs.last_y;
-#endif
-    data->point.x = touch_inputs.last_x;
-    data->point.y = touch_inputs.last_y;
-    data->state = touch_inputs.current_state;
-    OT_API_CALL(otCliOutputFormat("(DEBUG) (%s): X = %u Y = %u\r\n", pcTaskGetName(NULL), data->point.x, data->point.y));
 }
